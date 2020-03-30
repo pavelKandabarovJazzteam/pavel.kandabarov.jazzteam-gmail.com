80516c944b84bda3a0fc0ee057ffcd1874884ec2
Павел <pavel.Kandabarov.jazzteam@gmail.com>
2020-03-30 17:25:38 +0300

    Add JSDoc comment

--- numbers/js/script.js
+++ numbers/js/script.js
@@ -2,15 +2,29 @@ const arrayHundred = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
 const arrayTen = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 const arrayUnits = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
+
+/** @Returns the length of the entered number
+ * @param {number} number Input number
+ * @return {number}
+ */
 const numberLength = (number) => {
     return String(number).length;
 }
 
+
+/** @Convert hundreds of entered numbers to Roman
+ * @param {number} number Input number
+ * @return {number}
+ */
 const calculateHundred = (number) => {
     const hundred = number.toString()[0];
     return arrayHundred[hundred - 1];
 }
 
+/** @Convert ten of entered numbers to Roman
+ * @param {number} number Input number
+ * @return {number}
+ */
 const calculateTen = (length, number) => {
     let ten;
     if (length == 3) {
@@ -20,11 +34,15 @@ const calculateTen = (length, number) => {
     }
     if (ten == 0) {
         return "";
-    }else {
+    } else {
         return arrayTen[ten - 1];
     }
 }
 
+/** @Convert units of entered numbers to Roman
+ * @param {number} number Input number
+ * @return {number}
+ */
 const calculateUnits = (number) => {
     const unit = number % 10;
     if (unit == 0) {
@@ -34,7 +52,12 @@ const calculateUnits = (number) => {
     }
 }
 
-const calculate = function(number, length) {
+/** @Returns the entered number in Roman numerals
+ * @param {number} number Input number
+ * @param {number} length of input number
+ * @return {string}
+ */
+const calculate = (number, length) => {
     let result = "";
     switch (length) {
         case 3:
@@ -48,13 +71,16 @@ const calculate = function(number, length) {
     return result;
 }
 
+/** @Adds result to page
+ * @param {number} number The input number
+ */
 const addReuslt = (number) => {
     let add = document.querySelector("#add_text");
     add.innerText = "";
     add.append(calculate(number, numberLength(number)));
 }
 
-let input = document.querySelector("#input");
-input.oninput = function() {
+const input = document.querySelector("#input");
+document.querySelector("#input").oninput = function() {
     addReuslt(input.value);
 }
