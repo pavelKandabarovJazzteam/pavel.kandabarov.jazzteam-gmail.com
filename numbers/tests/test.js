28c4d9510f9debacbd5fe5c4ac53d1fa339b7b03
Павел <pavel.Kandabarov.jazzteam@gmail.com>
2020-04-10 10:53:24 +0300

    painted tests in more detail

--- numbers/tests/test.js
+++ numbers/tests/test.js
@@ -7,21 +7,33 @@ describe("Test of input number", function() {
     it("Input number Length", function() {
         assert.equal(numberLength(123), 3);
     });
+    it("Test for units function", function() {
+        assert.strictEqual(1, 1);
+    });
     for (let key in units) {
         it('Input ' + key + ", expected output " +units[key], function() {
-            assert.notStrictEqual(calculateUnits(key), key);
+            assert.strictEqual(calculateUnits(key), units[key]);
         });
     }
+    it("Test for tens function", function() {
+        assert.strictEqual(1, 1);
+    });
     for (let key in tens) {
         it('Input ' + key + ", expected output " +tens[key], function() {
             assert.strictEqual(calculateTen(key, 2), tens[key]);
         });
     }
+    it("Test for hundreds function", function() {
+        assert.strictEqual(1, 1);
+    });
     for (let key in hundreds) {
         it('Input ' + key + ", expected output " +hundreds[key], function() {
             assert.strictEqual(calculateTen(key), tens[key]);
         });
     }
+    it("Test for total function", function() {
+        assert.strictEqual(1, 1);
+    });
     for (let key in total) {
         it('Input ' + key + ", expected output " +total[key], function() {
             assert.strictEqual(calculate(key, 3), total[key]);
